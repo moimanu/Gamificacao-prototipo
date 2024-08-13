712444a747374e5a17a8b22444c9922bdf2d1d26
Moisés Emanuel <113472007+moimanu@users.noreply.github.com>
2024-08-13 16:50:51 -0300

    Update index.js

--- index.js
+++ index.js
@@ -6,3 +6,6 @@ function atualizarVH() {
     let vh = window.innerHeight * 0.01;
     document.documentElement.style.setProperty('--vh', `${vh}px`);
 }
+
+window.addEventListener('load', atualizarVH);
+window.addEventListener('resize', atualizarVH);
