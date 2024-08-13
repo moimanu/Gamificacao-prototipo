2de8e6b842b2c0690eaa5f1aba83ba1600f70ce9
Moisés Emanuel <113472007+moimanu@users.noreply.github.com>
2024-08-13 17:16:12 -0300

    Add files via upload

--- pages/pages.js
+++ pages/pages.js
@@ -1,3 +1,11 @@
 document.getElementById('bt_logout_id').addEventListener('click', function() {
     window.location.href = '../index.html';
 });
+
+function atualizarVH() {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+}
+
+window.addEventListener('load', atualizarVH);
+window.addEventListener('resize', atualizarVH);
