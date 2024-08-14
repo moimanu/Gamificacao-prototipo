254eb3c04df4a3dede2ce91514bba3f8d9680647
Moisés Emanuel <113472007+moimanu@users.noreply.github.com>
2024-08-13 23:45:38 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
-document.getElementById('bt_login_id').addEventListener('click', function() {
-    window.location.href = 'pages/ranking.html';
-});
+if(document.getElementById('bt_login_id')){
+    document.getElementById('bt_login_id').addEventListener('click', function() {
+        window.location.href = 'pages/ranking.html';
+    });
+}
+
+if(document.getElementById('bt_logout_id')){
+    document.getElementById('bt_logout_id').addEventListener('click', function() {
+        window.location.href = '../index.html';
+    });
+}
 
 function atualizarVH() {
     let vh = window.innerHeight * 0.01;
